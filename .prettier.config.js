f16703ede3637fb1e68a17a761726ed7393f2677
Hamza Abdalmajeed <54910400+itxch@users.noreply.github.com>
2024-09-14 21:54:09 +0100

    feat: publish

---
+++ .prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  experimentalTernaries: true,
+};
+
+export default config;
